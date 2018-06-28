1e235253f455c398f6ccf1c89f0fd9c555f5e6e9
Nelson Earle <nelson.earle137@gmail.com>
2018-06-28 18:05:32 -0500

    also cache external ip

--- files/pretty-header-data.sh
+++ files/pretty-header-data.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Weather
 w_url='http://rss.accuweather.com/rss/liveweather_rss.asp?metric=2&locCode=NAM|US|MO|LAKE%20SAINT%20LOUIS'
 w_sed='/Currently:/ s/.*: (.*): ([0-9]+)F.*/\2F, \1/p'
 curl -s "$w_url" | sed -rn "$w_sed" > /etc/update-motd.d/pretty-header-weather.txt
+
+# External ip
+curl -s 'https://icanhazip.com' > /etc/update-motd.d/pretty-header-ext-ip.txt
