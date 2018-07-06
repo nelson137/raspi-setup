f4ec86337baf26bb5ea0cd1ff2bed78b7c5d2056
Nelson Earle <nelson.earle137@gmail.com>
2018-07-06 10:12:25 -0500

    use http, not https, for ext ip

--- files/pretty-header-data.sh
+++ files/pretty-header-data.sh
@@ -6,4 +6,4 @@ w_sed='/Currently:/ s/.*: (.*): ([0-9]+)F.*/\2F, \1/p'
 curl -s "$w_url" | sed -rn "$w_sed" > /etc/update-motd.d/pretty-header-weather.txt
 
 # External ip
-curl -s 'https://icanhazip.com' > /etc/update-motd.d/pretty-header-ext-ip.txt
+curl -s 'http://icanhazip.com' > /etc/update-motd.d/pretty-header-ext-ip.txt
