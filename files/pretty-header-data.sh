4f6076f9876cc97e5bf849b5850bf1d4c59ba3e5
Nelson Earle <nelson.earle137@gmail.com>
2018-07-06 10:13:00 -0500

    try to use dig to get ext ip

--- files/pretty-header-data.sh
+++ files/pretty-header-data.sh
@@ -6,4 +6,9 @@ w_sed='/Currently:/ s/.*: (.*): ([0-9]+)F.*/\2F, \1/p'
 curl -s "$w_url" | sed -rn "$w_sed" > /etc/update-motd.d/pretty-header-weather.txt
 
 # External ip
-curl -s 'http://icanhazip.com' > /etc/update-motd.d/pretty-header-ext-ip.txt
+if which dig >/dev/null; then
+    ext_ip="$(dig +short myip.opendns.com @resolver1.opendns.com)"
+else
+    ext_ip="$(curl -s 'http://icanhazip.com')"
+fi
+echo "$ext_ip" > /etc/update-motd.d/pretty-header-ext-ip.txt
